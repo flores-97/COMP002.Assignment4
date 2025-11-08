4996861abd05dc40b79a35a641e025b725cf6d88
flores-97 <floresvictor15@gmail.com>
2025-11-08 10:47:51 -0800

    completed first question

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+function range(start, end) {
+    let listNum =[];// will be the array to list the numbers
+    for (let i = start; i <= end;i++) {//will take the 'start' to 'end' number and stay within the range
+        listNum.push(i)
+    }
+    return listNum;
+}
+
+console.log(range(1, 10));
+
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
